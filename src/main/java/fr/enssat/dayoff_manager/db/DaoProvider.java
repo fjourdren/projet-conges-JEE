721f6452ca4191f34e109fca718c923dfcf4709d
cforgeard <clement_forgeard@outlook.fr>
2019-11-15 19:10:41 +0100

    Edit DaoProvider to use Mock DAOs

--- src/main/java/fr/enssat/dayoff_manager/db/DaoProvider.java
+++ src/main/java/fr/enssat/dayoff_manager/db/DaoProvider.java
@@ -1,24 +1,24 @@
 package fr.enssat.dayoff_manager.db;
 
 import fr.enssat.dayoff_manager.db.dayoff.DayoffDao;
-import fr.enssat.dayoff_manager.db.dayoff.DayoffDaoImpl;
+import fr.enssat.dayoff_manager.db.dayoff.DayoffDaoMockImpl;
 import fr.enssat.dayoff_manager.db.dayoff_count.DayoffCountDao;
-import fr.enssat.dayoff_manager.db.dayoff_count.DayoffCountDaoImpl;
+import fr.enssat.dayoff_manager.db.dayoff_count.DayoffCountDaoMockImpl;
 import fr.enssat.dayoff_manager.db.dayoff_type.DayoffTypeDao;
-import fr.enssat.dayoff_manager.db.dayoff_type.DayoffTypeDaoImpl;
+import fr.enssat.dayoff_manager.db.dayoff_type.DayoffTypeDaoMockImpl;
 import fr.enssat.dayoff_manager.db.department.DepartmentDao;
-import fr.enssat.dayoff_manager.db.department.DepartmentDaoImpl;
+import fr.enssat.dayoff_manager.db.department.DepartmentDaoMockImpl;
 import fr.enssat.dayoff_manager.db.employee.EmployeeDao;
-import fr.enssat.dayoff_manager.db.employee.EmployeeDaoImpl;
+import fr.enssat.dayoff_manager.db.employee.EmployeeDaoMockImpl;
 
 public class DaoProvider {
 
     static {
-        DAYOFF_DAO = new DayoffDaoImpl();
-        DAYOFF_COUNT_DAO = new DayoffCountDaoImpl();
-        DAYOFF_TYPE_DAO = new DayoffTypeDaoImpl();
-        DEPARTMENT_DAO = new DepartmentDaoImpl();
-        EMPLOYEE_DAO = new EmployeeDaoImpl();
+        DAYOFF_DAO = new DayoffDaoMockImpl();
+        DAYOFF_COUNT_DAO = new DayoffCountDaoMockImpl();
+        DAYOFF_TYPE_DAO = new DayoffTypeDaoMockImpl();
+        DEPARTMENT_DAO = new DepartmentDaoMockImpl();
+        EMPLOYEE_DAO = new EmployeeDaoMockImpl();
     }
 
     private static final DayoffDao DAYOFF_DAO;
