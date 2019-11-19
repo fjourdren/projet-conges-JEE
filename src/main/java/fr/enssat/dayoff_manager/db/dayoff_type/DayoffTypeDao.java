62f31b7dc603a46458a4733108393557f7a88b9b
cforgeard <clement_forgeard@outlook.fr>
2019-11-19 22:03:41 +0100

    Add DayoffTypeDao.getAvailableDayoffTypes

--- src/main/java/fr/enssat/dayoff_manager/db/dayoff_type/DayoffTypeDao.java
+++ src/main/java/fr/enssat/dayoff_manager/db/dayoff_type/DayoffTypeDao.java
@@ -2,6 +2,14 @@ package fr.enssat.dayoff_manager.db.dayoff_type;
 
 import fr.enssat.dayoff_manager.db.GenericDao;
 
+import java.util.List;
+
 public interface DayoffTypeDao extends GenericDao<DayoffType> {
-    //nothing
+
+    /**
+     * Retourne tous les types sauf ceux marqués comme supprimé
+     *
+     * @return tous les types sauf ceux marqués comme supprimé
+     */
+    List<DayoffType> getAvailableDayoffTypes();
 }
--- src/main/java/fr/enssat/dayoff_manager/db/dayoff_type/DayoffTypeDaoImpl.java
+++ src/main/java/fr/enssat/dayoff_manager/db/dayoff_type/DayoffTypeDaoImpl.java
@@ -2,11 +2,19 @@ package fr.enssat.dayoff_manager.db.dayoff_type;
 
 import fr.enssat.dayoff_manager.db.GenericDaoImpl;
 
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 public class DayoffTypeDaoImpl extends GenericDaoImpl<DayoffType> implements DayoffTypeDao {
 
     public DayoffTypeDaoImpl() {
         super(DayoffType.class);
     }
 
-    //nothing
+    @Override
+    public List<DayoffType> getAvailableDayoffTypes() {
+        String qlString = "SELECT x FROM DayoffType x WHERE x.deleted = false";
+        TypedQuery<DayoffType> query = em.createQuery(qlString, DayoffType.class);
+        return query.getResultList();
+    }
 }
