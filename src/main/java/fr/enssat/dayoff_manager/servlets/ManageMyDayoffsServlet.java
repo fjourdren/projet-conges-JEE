1594f39ffff2673b88340b42cb398193aa555945
cforgeard <clement_forgeard@outlook.fr>
2019-11-20 23:05:28 +0100

    ManageMyDayoffsServlet

--- src/main/java/fr/enssat/dayoff_manager/servlets/ManageMyDayoffsServlet.java
+++ src/main/java/fr/enssat/dayoff_manager/servlets/ManageMyDayoffsServlet.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -49,39 +50,83 @@ public class ManageMyDayoffsServlet extends HttpServlet {
     @SuppressWarnings("deprecation")
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Date startDate = new Date();
-        String[] startDateParts = req.getParameter("start-date").split("-");
-        startDate.setYear(Integer.parseInt(startDateParts[0]));
-        startDate.setMonth(Integer.parseInt(startDateParts[1]));
-        startDate.setDate(Integer.parseInt(startDateParts[2]));
-        if (req.getParameter("start-date-type").equals("AFTERNOON")) {
-            startDate.setHours(12);
+        // check if user is connected
+        HttpSession session = req.getSession();
+        Employee employeeLogged = (Employee) session.getAttribute("employeeLogged");
+        if (employeeLogged == null || !(employeeLogged.getType() == EmployeeType.RH_ADMIN || employeeLogged.getType() == EmployeeType.RH)) {
+            resp.sendRedirect("default");
+            return;
         }
 
-        Date endDate = new Date();
-        String[] endDateParts = req.getParameter("end-date").split("-");
-        endDate.setYear(Integer.parseInt(endDateParts[0]));
-        endDate.setMonth(Integer.parseInt(endDateParts[1]));
-        endDate.setDate(Integer.parseInt(endDateParts[2]));
-        if (req.getParameter("end-date-type").equals("AFTERNOON")) {
-            endDate.setHours(12);
+        switch (req.getParameter("dayoff-action")) {
+            case "edit":
+                createAndSaveDayoffFromForm(req, employeeLogged);
+                break;
+            case "delete":
+                deleteDayoffWithForm(req);
+                break;
         }
 
-        Dayoff dayoff = new Dayoff();
-        dayoff.setStatus(DayoffStatus.WAITING);
-        dayoff.setDateStart(startDate);
-        dayoff.setDateEnd(endDate);
-        dayoff.setCommentRH(null);
+        resp.sendRedirect("manage-my-dayoffs");
+    }
+
+    /**
+     * Crée ou met à jour une demande de congé depuis un formulaire HTTP puis l'enregistre en base
+     *
+     * @param req      http request
+     * @param employee employé
+     */
+    private void createAndSaveDayoffFromForm(HttpServletRequest req, Employee employee) {
+        Dayoff dayoff;
+
+        if (req.getParameter("dayoff-id") == null || req.getParameter("dayoff-id").isEmpty()) {
+            dayoff = new Dayoff();
+            dayoff.setDateCreation(new Date());
+            dayoff.setStatus(DayoffStatus.WAITING);
+        } else {
+            dayoff = DaoProvider.getDayoffDao().findById(Long.parseLong(req.getParameter("dayoff-id")));
+        }
+
+        dayoff.setDateStart(convertHTMLDateToJavaDate(req.getParameter("start-date"), req.getParameter("start-date-type")));
+        dayoff.setDateEnd(convertHTMLDateToJavaDate(req.getParameter("end-date"), req.getParameter("end-date-type")));
         dayoff.setCommentEmployee(req.getParameter("employee-comment"));
+        dayoff.setType(DaoProvider.getDayoffTypeDao().findById(Long.parseLong(req.getParameter("dayoff-type"))));
+        dayoff.setEmployee(employee);
+        DaoProvider.getDayoffDao().save(dayoff);
+    }
 
+    /**
+     * Supprime une demande de congé depuis un formulaire HTTP
+     *
+     * @param req http request
+     */
+    private void deleteDayoffWithForm(HttpServletRequest req) {
+        Dayoff dayoff = DaoProvider.getDayoffDao().findById(Long.parseLong(req.getParameter("dayoff-id")));
+        DaoProvider.getDayoffDao().delete(dayoff);
+    }
 
-        String employeeComment = req.getParameter("employee-comment");
-        String typeID = req.getParameter("dayoff-type");
+    /**
+     * Convertit une date HTML (yyyy-mm-yy) en java.util.Date en prenant en compte le type de date (MORNING|AFTERNOON)
+     *
+     * @param date     date HTML (yyyy-mm-yy)
+     * @param dateType type de date (MORNING|AFTERNOON)
+     * @return java.util.Date
+     */
+    private Date convertHTMLDateToJavaDate(String date, String dateType) {
+        Calendar calendar = Calendar.getInstance();
+        String[] dateParts = date.split("-");
 
-        System.out.println(startDate);
-        System.out.println(endDate);
-        System.out.println(employeeComment);
-        System.out.println(typeID);
+        calendar.set(Calendar.YEAR, Integer.parseInt(dateParts[0]));
+        calendar.set(Calendar.MONTH, Integer.parseInt(dateParts[1]) - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateParts[2]));
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        if (dateType.equals("AFTERNOON")) {
+            calendar.set(Calendar.HOUR_OF_DAY, 12);
+        } else {
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+        }
 
+        return calendar.getTime();
     }
 }
