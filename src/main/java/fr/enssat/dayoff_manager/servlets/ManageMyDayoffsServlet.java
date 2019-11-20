2b9b93020bde2c724512182dbd9ddc4e278bcfe2
galeadon <alegalbtssio@gmail.com>
2019-11-20 12:27:28 +0100

    Ajout de fonction de traitmenet des jours

--- src/main/java/fr/enssat/dayoff_manager/servlets/ManageMyDayoffsServlet.java
+++ src/main/java/fr/enssat/dayoff_manager/servlets/ManageMyDayoffsServlet.java
@@ -15,9 +15,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @WebServlet(
         name = "ManageMyDayoffsServlet",
@@ -86,7 +89,7 @@ public class ManageMyDayoffsServlet extends HttpServlet {
 
     }
 
-    @Override
+
     protected boolean checkDayoff(Dayoff dayoff, Employee employee){
         List<Dayoff> dayoffs = DaoProvider.getEmployeeDao().getDayOffs(employee);
         for (Dayoff conge:dayoffs){
@@ -97,5 +100,50 @@ public class ManageMyDayoffsServlet extends HttpServlet {
 
         }
         return true;
+    }
+
+
+    protected float NbrDayoffOpen(Dayoff dayoff){
+        float nbrDay = getWorkingDaysBetweenTwoDates(dayoff.getDateStart(), dayoff.getDateEnd());
+        SimpleDateFormat formater = new SimpleDateFormat("hh");
+        String debutConge = formater.format(dayoff.getDateStart());
+        String finConge = formater.format(dayoff.getDateEnd());
+        if(debutConge.equals("12") && finConge.equals("00"))
+            return nbrDay-0.5f;
+        if(debutConge.equals("00") && finConge.equals("12"))
+            return nbrDay-0.5f;
+        return nbrDay;
+
+    }
+
+
+    protected int getWorkingDaysBetweenTwoDates(Date startDate, Date endDate) {
+        Calendar startCal = Calendar.getInstance();
+        startCal.setTime(startDate);
+
+        Calendar endCal = Calendar.getInstance();
+        endCal.setTime(endDate);
+
+        int workDays = 0;
+
+        //Return 0 if start and end are the same
+        if (startCal.getTimeInMillis() == endCal.getTimeInMillis()) {
+            return 0;
+        }
+
+        if (startCal.getTimeInMillis() > endCal.getTimeInMillis()) {
+            startCal.setTime(endDate);
+            endCal.setTime(startDate);
+        }
+
+        do {
+            //excluding start date
+            startCal.add(Calendar.DAY_OF_MONTH, 1);
+            if (startCal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && startCal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+                ++workDays;
+            }
+        } while (startCal.getTimeInMillis() < endCal.getTimeInMillis()); //excluding end date
+
+        return workDays;
     }
 }
